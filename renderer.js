c07383a8ce87a365633e1af71d3457a542e94615
minjung <tmzhftm88@etslight.co.kr>
2019-08-19 14:38:38 +0900

    Enthus 조명 driver 저항 설계

--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
-const test = document.getElementById('test');
-
-test.innerText = "hellow world222";
+function getOhm(){
+    var a = document.getElementById("ep").value;
+    var b = document.getElementById("led").value;
+    var c = document.getElementById("circuit").value;
+    
+    var ohm = 0.1/((a/c)/((3*b)+1));
+    document.getElementById("ohm").innerHTML = ohm;
+    }
