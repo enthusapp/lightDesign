8923ce78874a2f2bd60d9860f1342fd5497dfe1a
minjung <tmzhftm88@etslight.co.kr>
2019-08-20 16:25:06 +0900

    Enthus 조명 driver 저항 설계-addEventListener

--- renderer.js
+++ renderer.js
@@ -1,8 +1,11 @@
-function getOhm(){
-    var a = document.getElementById("ep").value;
-    var b = document.getElementById("led").value;
-    var c = document.getElementById("circuit").value;
-    
-    var ohm = 0.1/((a/c)/((3*b)+1));
-    document.getElementById("ohm").innerHTML = ohm;
+window.onload = function (){
+    var element = document.getElementById("button");
+    element.addEventListener("click", getOhm, false);
+    };
+    function getOhm(){
+        var a = document.getElementById("ep").value;
+        var b = document.getElementById("led").value;
+        var c = document.getElementById("circuit").value;
+        var ohm = 0.1/((a/c)/((3*b)+1));
+        document.getElementById("ohm").innerHTML = ohm;
     }
