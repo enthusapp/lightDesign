3e9aab739cc1cbf047c95e45663af7445f81e64a
minjung <tmzhftm88@etslight.co.kr>
2019-08-21 10:46:00 +0900

    Enthus 조명 driver 저항 설계-ESLint_2

--- renderer.js
+++ renderer.js
@@ -1,11 +1,11 @@
-window.onload = function (){
-    var element = document.getElementById("button");
-    element.addEventListener("click", getOhm, false); 
-    };
-    function getOhm(){
-        var a = document.getElementById("ep").value;
-        var b = document.getElementById("led").value;
-        var c = document.getElementById("circuit").value;
-        var ohm = 0.1/((a/c)/((3*b)+1));
-        document.getElementById("ohm").innerHTML = ohm;
-    }
+window.onload = function main() {
+  const element = document.getElementById('button');
+  element.addEventListener('click', getOhm, false);
+};
+function getOhm() {
+  const a = document.getElementById('ep').value;
+  const b = document.getElementById('led').value;
+  const c = document.getElementById('circuit').value;
+  const ohm = 0.1 / ((a / c) / ((3 * b) + 1));
+  document.getElementById('ohm').innerHTML = ohm;
+}
