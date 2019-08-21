4fc55ef8f93f79f8e051ce3a8a0b6a0e6400740d
hando <enthusapp@gmail.com>
2019-08-21 11:16:45 +0900

    optimization

--- renderer.js
+++ renderer.js
@@ -1,11 +1,9 @@
-window.onload = function main() {
-  const element = document.getElementById('button');
-  element.addEventListener('click', getOhm, false);
-};
-function getOhm() {
-  const a = document.getElementById('ep').value;
-  const b = document.getElementById('led').value;
-  const c = document.getElementById('circuit').value;
-  const ohm = 0.1 / ((a / c) / ((3 * b) + 1));
-  document.getElementById('ohm').innerHTML = ohm;
-}
+document
+  .getElementById('button')
+  .addEventListener('click', () => {
+    const ep = document.getElementById('ep').value;
+    const led = document.getElementById('led').value;
+    const circuit = document.getElementById('circuit').value;
+    const ohm = 0.1 / ((ep / circuit) / ((3 * led) + 1));
+    document.getElementById('ohm').innerHTML = ohm;
+  });
