861a8808bc27e079a44ae6b52a32dd80b382de03
minjung <tmzhftm88@etslight.co.kr>
2019-08-22 11:17:13 +0900

    Enthus 조명 driver 저항 설계-ESLint_3
    입력 완료 버튼 클릭 시 결과가 보이도록 설계

--- renderer.js
+++ renderer.js
@@ -5,5 +5,6 @@ document
     const led = document.getElementById('led').value;
     const circuit = document.getElementById('circuit').value;
     const ohm = 0.1 / ((ep / circuit) / ((3 * led) + 1));
-    document.getElementById('ohm').innerHTML = ohm;
+    document.getElementById('ohm')
+      .innerHTML = `최적 저항은 <b>${ohm}</b> 입니다.`;
   });
