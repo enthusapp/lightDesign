d857efbc1868d27d557cc1a6fac7f84dd9517c36
minjung <tmzhftm88@etslight.co.kr>
2019-08-26 14:35:17 +0900

    Enthus 조명 driver 저항 설계-ESLint_4
    CSS, FORM 사용

--- renderer.js
+++ renderer.js
@@ -1,26 +1,20 @@
 document
-  .getElementById('formTest')
+  .getElementById('form1')
   .addEventListener('submit', (e) => {
     e.preventDefault();
     e.stopPropagation();
-    const data = new FormData(e.currentTarget);
-    console.log(data.get('ep'));
-    console.log(data.get('led'));
-    console.log(data.get('circuit'));
-  });
-
-/*
-document
-  .getElementById('button')
-  .addEventListener('click', () => {
-    const ep = Number(document.getElementById('ep').value);
-    const led = Number(document.getElementById('led').value);
-    const circuit = Number(document.getElementById('circuit').value);
+    const form1 = new FormData(document.getElementById('form1'));
+    const ep = form1.get('ep');
+    const led = form1.get('led');
+    const circuit = form1.get('circuit');
 
     if (!ep || !led || !circuit) return;
 
     const ohm = 0.1 / ((ep / circuit) / ((3 * led) + 1));
     document.getElementById('ohm')
       .innerHTML = `최적 저항은 <b>${ohm}</b> 입니다.`;
+
+    console.log(form1.get('ep'));
+    console.log(form1.get('led'));
+    console.log(form1.get('circuit'));
   });
-*/
