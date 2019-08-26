06cecae9413c620abb6d6af49c4d5f93cb74075d
hando <enthusapp@gmail.com>
2019-08-26 14:58:43 +0900

    optimization

--- renderer.js
+++ renderer.js
@@ -3,18 +3,14 @@ document
   .addEventListener('submit', (e) => {
     e.preventDefault();
     e.stopPropagation();
-    const form1 = new FormData(document.getElementById('form1'));
-    const ep = form1.get('ep');
-    const led = form1.get('led');
-    const circuit = form1.get('circuit');
+    const f = new FormData(e.currentTarget());
+    let ohm;
 
-    if (!ep || !led || !circuit) return;
+    ohm = f.get('ep') / f.get('circuit');
+    ohm /= (3 * f.get('led')) + 1;
+    ohm = 0.1 / ohm;
 
-    const ohm = 0.1 / ((ep / circuit) / ((3 * led) + 1));
-    document.getElementById('ohm')
+    document
+      .getElementById('ohm')
       .innerHTML = `최적 저항은 <b>${ohm}</b> 입니다.`;
-
-    console.log(form1.get('ep'));
-    console.log(form1.get('led'));
-    console.log(form1.get('circuit'));
   });
