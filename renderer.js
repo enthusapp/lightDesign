cf45fe53e70179be1184fc31aa8f9b9ac94bd23a
minjung <tmzhftm88@etslight.co.kr>
2020-07-20 16:45:46 +0900

    update

--- renderer.js
+++ renderer.js
@@ -30,13 +30,9 @@ document
 
     if (f.get('driver') === 'aur6601') {
       currentForOneLED = f.get('ep') / f.get('circuit') / ((3 * f.get('led')) + 1);
-    } if (f.get('driver') === 'lm317') {
-      currentForOneLED = f.get('ep') / f.get('circuit') / 24;
-    }
-
-    if (f.get('driver') === 'aur6601') {
       ohm = 0.1 / currentForOneLED;
-    } if (f.get('driver') === 'lm317') {
+    } else {
+      currentForOneLED = f.get('ep') / f.get('circuit') / 24;
       ohm = 1.25 / currentForOneLED;
     }
 
