acd89dc9008dbbe18665343ac4ab1176edb39980
minjung <tmzhftm88@etslight.co.kr>
2020-09-01 09:31:37 +0900

    https://github.com/enthusapp/lightDesign/issues/10

--- renderer.js
+++ renderer.js
@@ -17,34 +17,47 @@ const decimalAdjust = (type, val, ex) => {
   return +`${value[0]}e${value[1] ? +value[1] + exp : exp}`;
 };
 
-document
-  .getElementById('form1')
-  .addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+window.onload = () => {
+  const url = (new URL(window.location.href)).searchParams;
+  const driver = url.get('driver');
+  const ep = url.get('ep');
+  const circuit = url.get('circuit');
+  const led = url.get('led');
+  const driverArr = document.getElementsByName('driver');
 
-    const f = new FormData(e.currentTarget);
+  let currentForOneLED;
+  let ohm;
+  if (driver === 'aur6601') {
+    currentForOneLED = ep / circuit / ((3 * led) + 1);
+    ohm = 0.1 / currentForOneLED;
+  } else {
+    currentForOneLED = ep / circuit / 24;
+    ohm = 1.25 / currentForOneLED;
+  }
 
-    let currentForOneLED;
-    let ohm;
-
-    if (f.get('driver') === 'aur6601') {
-      currentForOneLED = f.get('ep') / f.get('circuit') / ((3 * f.get('led')) + 1);
-      ohm = 0.1 / currentForOneLED;
-    } else {
-      currentForOneLED = f.get('ep') / f.get('circuit') / 24;
-      ohm = 1.25 / currentForOneLED;
+  driverArr.forEach((e) => {
+    if (e.value === driver) {
+      e.checked = true;
     }
+  });
 
+  document
+    .getElementById('ep').value = `${ep}`;
+  document
+    .getElementById('led').value = `${led}`;
+  document
+    .getElementById('circuit').value = `${circuit}`;
+
+  if (ohm) {
     document
       .getElementById('ohm')
       .innerHTML = `최적 저항은 <b>${
         decimalAdjust('round', ohm, -2)
       }</b> Ω 입니다.`;
-
     document
       .getElementById('current')
       .innerHTML = `전체 전류는 <b>${
-        decimalAdjust('round', currentForOneLED * f.get('led'), -2)
+        decimalAdjust('round', currentForOneLED * led, -2)
       }</b> A 입니다.`;
-  });
+  }
+};
